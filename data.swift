f5b9c78a083922d2acff81d823f581c64a3a6459
lycheelichi <62992415+lycheelichi@users.noreply.github.com>
2023-09-25 07:54:03 +0900

    Create data.swift

---
+++ data.swift
@@ -0,0 +1,6 @@
+
+// array you need for the List of Restaurant
+
+var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery"
+, "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+

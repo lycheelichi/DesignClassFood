54a7138d70cc799f89550052155528b1aa82db6a
lycheelichi <62992415+lycheelichi@users.noreply.github.com>
2023-09-25 08:04:45 +0900

    Update data.swift

--- data.swift
+++ data.swift
@@ -7,3 +7,9 @@ var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite 
 
 var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
 
+var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney",
+"New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"];
+
+
+var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+
